a0703f59cef33aa7399998b7544131d1051b55fa
M Craciunescu <m.craciunescu@tarent.de>
2018-08-17 21:52:02 +0300

    Added first test

--- scripts/generate_coverage.sh
+++ scripts/generate_coverage.sh
@@ -7,7 +7,7 @@ i=0
 for dir in $(find . -maxdepth 10 -not -path './.git*' -not -path '*/_test.go' -type d);
 do
     if ls ${dir}/*.go &> /dev/null; then
-        GO_TEST_DISABLED=true go test -v -covermode=atomic -coverprofile=./cov/$i.out ./${dir}
+        go test -v -covermode=atomic -coverprofile=./cov/$i.out ./${dir}
         i=$((i+1))
     fi
 done
